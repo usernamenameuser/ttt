d82cdae3e8d17cb463ea22c2fe3248cf764d8069
user <user@MacBook-Pro-user.local>
2017-02-20 13:42:39 +0200

    Initial Commit

---
+++ IVDomZad2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IVDomZad2
+//
+//  Created by user on 20.02.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
